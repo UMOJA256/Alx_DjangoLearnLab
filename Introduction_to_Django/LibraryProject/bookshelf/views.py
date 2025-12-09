735ad04e9dc3d8dce9df34b3f41ac94af464b9d2
UMOJA256 <mugisha25steven@gmail.com>
2025-12-09 05:55:46 -0800

    Add Introduction_to_Django

---
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Book
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/index.html', {'books': books})
